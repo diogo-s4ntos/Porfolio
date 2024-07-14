512ede9cc303c9dc29bb9c18a36d7a83ce4f6fba
Diogo Santos <diogomariasantos3@gmail.com>
2024-07-14 18:11:44 +0100

    Deleted experience.js

--- JS/experiences.js
+++
@@ -1,6 +0,0 @@
-function firstExperience() {
-    document.getElementById('experience-title').textContent = "Developer in Training at CGI"
-    document.getElementById('experience-description').textContent = "Currently, I am thriving in my role as a Developer in Training at CGI, where I am immersed in the dynamic world of software development. This experience has provided me with invaluable hands-on training and exposure to cutting-edge technologies. I actively engage in coding, problem-solving, and collaborating with seasoned professionals, contributing to the development of innovative solutions. My time at CGI serves as a pivotal chapter in my journey, as I continue to refine my skills and stay abreast of industry trends. The supportive environment and mentorship at CGI have not only accelerated my learning curve but also reinforced my passion for transforming ideas into impactful digital solutions. This role aligns seamlessly with my dedication to excellence in web development, making every coding challenge a rewarding step forward in my professional growth."
-    document.getElementById("experience-1").style.boxShadow = "none"
-    document.getElementById("experience-2").style.boxShadow = "0px 0px 10px var(--secondary)"
-}
