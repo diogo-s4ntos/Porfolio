b42411ba7b3c0295063fd9e4cfe79fb2cb69b68a
Diogo Santos <diogomariasantos3@gmail.com>
2024-07-14 17:53:51 +0100

    Updated close menu button

--- JS/header.js
+++ JS/header.js
@@ -1,13 +1,14 @@
 function openPhoneMenu(checkbox) {
     if (checkbox.checked) {
-        document.body.style.overflow = "hidden";
+        document.body.style.overflow = "hidden"
         document.getElementById('phone-menu').style.transform = 'translateX(0%)'
     } else {
-        document.body.style.overflow = "scroll";
+        document.body.style.overflow = "scroll"
         document.getElementById('phone-menu').style.transform = 'translateX(100%)'
     }
 }
 
 function closeMenu() {
     document.getElementById('phone-menu').style.transform = 'translateX(100%)'
+    document.getElementById('menu-button').checked = false
 }
